3af2957e2a335d3bd0ea93ce7d1a60c4cda860cb
Fargano <fargano@amazon.com>
2021-04-03 17:01:33 -0500

    added IAM policy

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "aws_iam_policy" "policy" {
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
-  policy = << EOF
+  policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
