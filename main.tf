6cceb334a333a9ea37c1eb5b1f39b7212d55254a
Fargano <fargano@amazon.com>
2021-04-03 12:41:01 -0500

    added kub provider

--- main.tf
+++ main.tf
@@ -2,6 +2,22 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.12"
+}
+
 # Module to deploy out full network infrastructure
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
